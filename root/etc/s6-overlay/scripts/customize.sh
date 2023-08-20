b82fffdc589e863564967399d8f0dfc2dc1657fc
mfiscus <matt.fiscus@mac.com>
2023-08-19 19:19:24 -0700

    updates to Dockerfile and customize.sh
    Updates include support for SSL enabled domains hosted behind a reverse proxy. Created functions for configuration file modifications.

--- root/etc/s6-overlay/scripts/customize.sh
+++ root/etc/s6-overlay/scripts/customize.sh
@@ -10,6 +10,7 @@ if [ -e /.firstRunComplete ]; then
 
 fi
 
+
 # Make sure environment variables are set
 if [ -z ${URL:-} ]; then
   echo "URL not set"
@@ -17,82 +18,248 @@ if [ -z ${URL:-} ]; then
 
 fi
 
+
+# Function to edit urfd.ini values
+# $1=Section
+# $2=Property
+# $3=Value
+function __edit_value() {
+    if [ ${#} -eq 3 ]; then
+        local section=${1}
+        local property=${2}
+        local value=${3}
+
+        sed -i "/^\[${section}\]/,/^\[/{s'\(^${property}[[:blank:]]*=[[:blank:]]*\)\([[:print:]]*\)'\1${value}'}" ${URFD_CONFIG_TMP_DIR}/urfd.ini
+
+        return
+
+    else
+        exit 1
+
+    fi
+
+}
+
+
+# Function to edit rename urfd.ini properties
+# $1=Old property
+# $2=New Property
+function __edit_property() {
+    if [ ${#} -eq 2 ]; then
+        local old_property=${1}
+        local new_property=${2}
+        local file=${URFD_CONFIG_TMP_DIR}/urfd.ini
+
+        sed -i "s/${old_property}/${new_property}/g" ${file}
+
+        return
+
+    else
+        exit 1
+
+    fi
+
+}
+
+
+# Function to comment out matched line
+# $1=Section
+# $2=Property
+function __line_comment() {
+    if [ ${#} -eq 2 ]; then
+        local section=${1}
+        local property=${2}
+        local file=${URFD_CONFIG_TMP_DIR}/urfd.ini
+
+        sed -i "/^\[${section}\]/,/^\[/{s'\(^${property}*\)'#\ \1'}" ${file}
+
+        return
+
+    else
+        exit 1
+
+    fi
+
+}
+
+
+# Function to customize index.php
+# $1=search
+# $2=replace
+function __edit_dashboard() {
+    if [ ${#} -eq 2 ]; then
+        local search=${1}
+        local replace=${2}
+        local file=${URFD_WEB_DIR}/index.php
+
+        sed -i "s,${search},${replace},g" ${file}
+
+        return
+
+    else
+        exit 1
+
+    fi
+    
+}
+
+
+# Function to edit values in config.inc.php
+# $1=search
+# $2=replace
+function __edit_config() {
+    if [ ${#} -eq 2 ]; then
+        local search=${1}
+        local replace=${2}
+        local file=${URFD_WEB_DIR}/pgs/config.inc.php
+
+        sed -i "s,${search},\1${replace},g" ${file}
+
+        return
+
+    else
+        exit 1
+
+    fi
+    
+}
+
+
+# Function to delete matched line from files
+# $1 = match for line to delete
+# $2 = file
+function __delete_line() {
+    if [ ${#} -eq 2 ]; then
+        local delete=${1}
+        local file=${URFD_WEB_DIR}/${2}
+
+        sed -i "/${delete}/d" ${file}
+
+        return
+
+    else
+        exit 1
+
+    fi
+
+}
+
+
+# Function to fix dates (convert to american)
+# $1 = file
+function __fix_date() {
+    if [ ${#} -eq 1 ]; then
+        local file=${URFD_WEB_DIR}/${1}
+
+        sed -i "s/d\.m\.Y/m\.d\.Y/g" ${file}
+
+        return
+
+    else
+        exit 1
+
+    fi
+
+}
+
+
 # configure dashboard
 if [ ! -z ${CALLSIGN:-} ]; then
-  sed -i "s/\(PageOptions\['MetaAuthor'\][[:blank:]]*\=[[:blank:]]*\)'\([[:alnum:]]*\)'/\1\'${CALLSIGN}\'/g" ${URFD_DASH_CONFIG} # callsign
+  __edit_config "\(PageOptions\['MetaAuthor'\][[:blank:]]*\=[[:blank:]]*\)'\([[:alnum:]]*\)'" "'${CALLSIGN}'" # callsign
 
 fi
+
 
 if [ ! -z ${EMAIL:-} ]; then
-  sed -i "s/\(PageOptions\['ContactEmail'\][[:blank:]]*\=[[:blank:]]*\)'\([[:print:]]*\)'/\1\'${EMAIL}\'/g" ${URFD_DASH_CONFIG} # email address
+  __edit_config "\(PageOptions\['ContactEmail'\][[:blank:]]*\=[[:blank:]]*\)'\([[:print:]]*\)'" "'${EMAIL}'" # email address
 
 fi
 
-sed -i "s/\(CallingHome\['Country'\][[:blank:]]*\=[[:blank:]]*\)\"\([[:print:]]*\)\"/\1\"$(echo ${COUNTRY} | awk '{gsub(/ /,"\\ ")}8')\"/g" ${URFD_DASH_CONFIG} # country
-sed -i "s/\(CallingHome\['Comment'\][[:blank:]]*\=[[:blank:]]*\)\"\([[:print:]]*\)\"/\1\"$(echo ${DESCRIPTION} | awk '{gsub(/ /,"\\ ")}8')\"/g" ${URFD_DASH_CONFIG} # description
-sed -i "s/\(CallingHome\['MyDashBoardURL'\][[:blank:]]*\=[[:blank:]]*\)'\([[:print:]]*\)'/\1\'http:\/\/${URL}:${PORT}\/\'/g" ${URFD_DASH_CONFIG} # URL
-sed -i "s/\(CallingHome\['Active'\][[:blank:]]*\=[[:blank:]]*\)[[:alpha:]]*/\1${CALLHOME}/g" ${URFD_DASH_CONFIG} # call home active
-sed -i "s/\(PageOptions\['NumberOfModules'\][[:blank:]]*\=[[:blank:]]*\)[[:digit:]]*/\1${NUM_MODULES}/g" ${URFD_DASH_CONFIG} # number of modules
-sed -i "s/\(PageOptions\['ModuleNames'\]\['A'\][[:blank:]]*\=[[:blank:]]*\)'\([[:print:]]*\)'/\1\'${MODULEA}\'/g" ${URFD_DASH_CONFIG} # name module A
-sed -i "s/\(PageOptions\['ModuleNames'\]\['B'\][[:blank:]]*\=[[:blank:]]*\)'\([[:print:]]*\)'/\1\'${MODULEB}\'/g" ${URFD_DASH_CONFIG} # name module B
-sed -i "s/\(PageOptions\['ModuleNames'\]\['C'\][[:blank:]]*\=[[:blank:]]*\)'\([[:print:]]*\)'/\1\'${MODULEC}\'/g" ${URFD_DASH_CONFIG} # name module C
-sed -i "s/\(PageOptions\['ModuleNames'\]\['D'\][[:blank:]]*\=[[:blank:]]*\)'\([[:print:]]*\)'/\1\'${MODULED}\'/g" ${URFD_DASH_CONFIG} # name module D
-sed -i "s/\(PageOptions\['RepeatersPage'\]\['IPModus'\][[:blank:]]*\=[[:blank:]]*\)'\([[:print:]]*\)'/\1\'HideIP\'/g" ${URFD_DASH_CONFIG} # Hide IP addresses on repeaters page
-sed -i "s/\(PageOptions\['PeerPage'\]\['IPModus'\][[:blank:]]*\=[[:blank:]]*\)'\([[:print:]]*\)'/\1\'HideIP\'/g" ${URFD_DASH_CONFIG} # Hide IP addresses on peer page
-sed -i "s/\(PageOptions\['CustomTXT'\][[:blank:]]*\=[[:blank:]]*\)'\([[:print:]]*\)'/\1'$(echo ${DESCRIPTION} | awk '{gsub(/ /,"\\ ")}8')'/g" ${URFD_DASH_CONFIG} # Add description text
-sed -i "s/\(PageOptions\['IRCDDB'\]\['Show'\][[:blank:]]*\=[[:blank:]]*\)[[:alpha:]]*/\1false/g" ${URFD_DASH_CONFIG} # Hide IRCDDB page
-sed -i "s,<?php echo \$Reflector->GetReflectorName(); ?> Multiprotocol Reflector,<a href=\"http://${URL}:${PORT}/\"><img src=\"/img/logo.png\" alt=\"CHRC Logo\" width=\"50\" height=\"50\"></a>,g" ${URFD_WEB_DIR}/index.php # add custom logo
-sed -i "/_GET\['show'\]\ ==\ \"livequadnet\"/d" ${URFD_WEB_DIR}/index.php # remove livequadnet
-sed -i "/show=livequadnet/d" ${URFD_WEB_DIR}/index.php # remove livequadnet
-sed -i "s/d\.m\.Y/m\/d\/Y/g" ${URFD_WEB_DIR}/pgs/peers.php # convert date format to US
-sed -i "s/d\.m\.Y/m\/d\/Y/g" ${URFD_WEB_DIR}/pgs/repeaters.php # convert date format to US
-sed -i "s/d\.m\.Y/m\/d\/Y/g" ${URFD_WEB_DIR}/pgs/users.php # convert date format to US
+
+if [[ ${SSL:-} == "true" ]]; then
+  __edit_dashboard "<?php echo \$Reflector->GetReflectorName(); ?> Multiprotocol Reflector" "<a href=\"https://${URL}/\"><img src=\"/img/logo.png\" alt=\"CHRC Logo\" width=\"50\" height=\"50\"></a>" # add custom logo
+  __edit_config "\(CallingHome\['MyDashBoardURL'\][[:blank:]]*\=[[:blank:]]*\)'\([[:print:]]*\)'" "'https:\/\/${URL}'" # URL
+
+else
+  __edit_dashboard "<?php echo \$Reflector->GetReflectorName(); ?> Multiprotocol Reflector" "<a href=\"http://${URL}:${PORT}/\"><img src=\"/img/logo.png\" alt=\"CHRC Logo\" width=\"50\" height=\"50\"></a>" # add custom logo
+  __edit_config "\(CallingHome\['MyDashBoardURL'\][[:blank:]]*\=[[:blank:]]*\)'\([[:print:]]*\)'" "'http:\/\/${URL}:${PORT}'" | grep "MyDashBoardURL" # URL
+
+fi
+
+
+__edit_config "\(CallingHome\['Country'\][[:blank:]]*\=[[:blank:]]*\)\"\([[:print:]]*\)\"" "\"$(echo ${COUNTRY} | awk '{gsub(/ /,"\\ ")}8')\"" # country
+__edit_config "\(CallingHome\['Comment'\][[:blank:]]*\=[[:blank:]]*\)\"\([[:print:]]*\)\"" "\"$(echo ${DESCRIPTION} | awk '{gsub(/ /,"\\ ")}8')\"" # comment
+__edit_config "\(CallingHome\['Active'\][[:blank:]]*\=[[:blank:]]*\)[[:alpha:]]*" "${CALLHOME}" # calling home toggle
+__edit_config "\(Service\['XMLFile'\][[:blank:]]*\=[[:blank:]]*\)'\([[:print:]]*\)'" "'${URFD_CONFIG_DIR}/xlxd.xml'" # path to xml file
+__edit_config "\(CallingHome\['HashFile'\][[:blank:]]*\=[[:blank:]]*\)\"\([[:print:]]*\)\"" "\"${URFD_CONFIG_DIR}/callinghome.php\"" # path to callinghome hash file
+__edit_config "\(CallingHome\['LastCallHomefile'\][[:blank:]]*\=[[:blank:]]*\)\"\([[:print:]]*\)\"" "\"${URFD_CONFIG_DIR}\/lastcallhome.php\"" # path to lastcallhome file
+__edit_config "\(CallingHome\['InterlinkFile'\][[:blank:]]*\=[[:blank:]]*\)\"\([[:print:]]*\)\"" "\"${URFD_CONFIG_DIR}\/xlxd.interlink\"" # path to interlink file
+__edit_config "\(PageOptions\['ModuleNames'\]\['A'\][[:blank:]]*\=[[:blank:]]*\)'\([[:print:]]*\)'" "'${MODULEA}'" # name module A
+__edit_config "\(PageOptions\['ModuleNames'\]\['B'\][[:blank:]]*\=[[:blank:]]*\)'\([[:print:]]*\)'" "'${MODULEB}'" # name module B
+__edit_config "\(PageOptions\['ModuleNames'\]\['C'\][[:blank:]]*\=[[:blank:]]*\)'\([[:print:]]*\)'" "'${MODULEC}'" # name module C
+__edit_config "\(PageOptions\['ModuleNames'\]\['D'\][[:blank:]]*\=[[:blank:]]*\)'\([[:print:]]*\)'" "'${MODULED}'" # name module D
+__edit_config "\(PageOptions\['RepeatersPage'\]\['IPModus'\][[:blank:]]*\=[[:blank:]]*\)'\([[:print:]]*\)'" "'HideIP'" # hide IP address on repeaters page
+__edit_config "\(PageOptions\['PeerPage'\]\['IPModus'\][[:blank:]]*\=[[:blank:]]*\)'\([[:print:]]*\)'" "'HideIP'" # hide IP address on peers page
+__delete_line "show=livequadnet" "index.php" # remove livequadnet
+__delete_line "_GET\['show'\]\ ==\ \"livequadnet\"" "index.php" # remove livequadnet
+__fix_date "pgs/users.php" # convert date format to US
+__fix_date "pgs/peers.php" # convert date format to US
+__fix_date "pgs/repeaters.php" # convert date format to US
 
 
 # install configuration files
 if [[ -e ${URFD_CONFIG_DIR:-} ]] && [[ -e ${URFD_CONFIG_TMP_DIR:-} ]]; then
   IP=$( hostname -I )
-  sed -i "/\[Names\]/{n;s/\(Callsign[[:blank:]]*\=[[:blank:]]*\)URF[[:print:]]*/\1${URFNUM}/;}" ${URFD_CONFIG_TMP_DIR}/urfd.ini
-  sed -i "s/\(SysopEmail[[:blank:]]*\=[[:blank:]]*\)[[:print:]]*/\1${EMAIL}/g" ${URFD_CONFIG_TMP_DIR}/urfd.ini
-  sed -i "s/\(Country[[:blank:]]*\=[[:blank:]]*\)[[:print:]]*/\1${COUNTRY}/g" ${URFD_CONFIG_TMP_DIR}/urfd.ini
-  sed -i "s/\(Sponsor[[:blank:]]*\=[[:blank:]]*\)[[:print:]]*/\1${DESCRIPTION}/g" ${URFD_CONFIG_TMP_DIR}/urfd.ini
-  sed -i "s/\(DashboardUrl[[:blank:]]*\=[[:blank:]]*\)[[:print:]]*/\1http:\/\/${URL}:${PORT}/g" ${URFD_CONFIG_TMP_DIR}/urfd.ini
-  sed -i "s/\(^Modules[[:blank:]]*\=[[:blank:]]*\)[[:alpha:]]*/\1${MODULES}/g" ${URFD_CONFIG_TMP_DIR}/urfd.ini
-  sed -i "s/\(DescriptionA[[:blank:]]*\=[[:blank:]]*\)[[:print:]]*/\1${MODULEA}/g" ${URFD_CONFIG_TMP_DIR}/urfd.ini
-  sed -i "s/DescriptionD/DescriptionB/g" ${URFD_CONFIG_TMP_DIR}/urfd.ini
-  sed -i "s/\(DescriptionB[[:blank:]]*\=[[:blank:]]*\)[[:print:]]*/\1${MODULEB}/g" ${URFD_CONFIG_TMP_DIR}/urfd.ini
-  sed -i "s/DescriptionM/DescriptionC/g" ${URFD_CONFIG_TMP_DIR}/urfd.ini
-  sed -i "s/\(DescriptionC[[:blank:]]*\=[[:blank:]]*\)[[:print:]]*/\1${MODULEC}/g" ${URFD_CONFIG_TMP_DIR}/urfd.ini
-  sed -i "s/DescriptionS/DescriptionD/g" ${URFD_CONFIG_TMP_DIR}/urfd.ini
-  sed -i "s/\(DescriptionD[[:blank:]]*\=[[:blank:]]*\)[[:print:]]*/\1${MODULED}/g" ${URFD_CONFIG_TMP_DIR}/urfd.ini
-  sed -i "/\[Brandmeister\]/{n;s/\(Enable[[:blank:]]*\=[[:blank:]]*\)[[:alpha:]]*/\1${BRANDMEISTER}/;}" ${URFD_CONFIG_TMP_DIR}/urfd.ini
-  sed -i "/\[USRP\]/{n;s/\(Enable[[:blank:]]*\=[[:blank:]]*\)[[:alpha:]]*/\1${ALLSTAR}/;}" ${URFD_CONFIG_TMP_DIR}/urfd.ini
-  sed -i "s/\(IPAddress[[:blank:]]*\=[[:blank:]]*\)[[:print:]]*/\1${IP}/g" ${URFD_CONFIG_TMP_DIR}/urfd.ini
-  sed -i "s/\(RegistrationID[[:blank:]]*\=[[:blank:]]*\)[[:digit:]]*/\1${YSFID}/g" ${URFD_CONFIG_TMP_DIR}/urfd.ini
-  sed -i "s/\(RegistrationName[[:blank:]]*\=[[:blank:]]*\)[[:print:]]*/\1${URFNUM}/g" ${URFD_CONFIG_TMP_DIR}/urfd.ini
-  sed -i "s/\(RegistrationDescription[[:blank:]]*\=[[:blank:]]*\)[[:print:]]*/\1${DESCRIPTION}/g" ${URFD_CONFIG_TMP_DIR}/urfd.ini
-  sed -i "s'\(^FilePath[[:blank:]]*\=[[:blank:]]*\)[[:print:]]*dmrid.dat'\1${URFD_CONFIG_DIR}/dmrid.dat'g" ${URFD_CONFIG_TMP_DIR}/urfd.ini
-  sed -i "s'\(^FilePath[[:blank:]]*\=[[:blank:]]*\)[[:print:]]*nxdn.dat'\1${URFD_CONFIG_DIR}/nxdn.dat'g" ${URFD_CONFIG_TMP_DIR}/urfd.ini
-  sed -i "s'\(^FilePath[[:blank:]]*\=[[:blank:]]*\)[[:print:]]*ysfnode.dat'\1${URFD_CONFIG_DIR}/ysfnode.dat'g" ${URFD_CONFIG_TMP_DIR}/urfd.ini
-  sed -i "s'\(XmlPath[[:blank:]]*\=[[:blank:]]*\)[[:print:]]*xlxd.xml'\1${URFD_CONFIG_DIR}/xlxd.xml'g" ${URFD_CONFIG_TMP_DIR}/urfd.ini
-  sed -i "s,\(Service\['XMLFile'\][[:blank:]]*\=[[:blank:]]*\)'\([[:print:]]*\)',\1'${URFD_CONFIG_DIR}/xlxd.xml',g" ${URFD_DASH_CONFIG}
-  sed -i "s'\(WhitelistPath[[:blank:]]*\=[[:blank:]]*\)[[:print:]]*urfd.whitelist'\1${URFD_CONFIG_DIR}/urfd.whitelist'g" ${URFD_CONFIG_TMP_DIR}/urfd.ini
-  sed -i "s'\(BlacklistPath[[:blank:]]*\=[[:blank:]]*\)[[:print:]]*urfd.blacklist'\1${URFD_CONFIG_DIR}/urfd.blacklist'g" ${URFD_CONFIG_TMP_DIR}/urfd.ini
-  sed -i "s'\(InterlinkPath[[:blank:]]*\=[[:blank:]]*\)[[:print:]]*urfd.interlink'\1${URFD_CONFIG_DIR}/urfd.interlink'g" ${URFD_CONFIG_TMP_DIR}/urfd.ini
-  sed -i "s'\(G3TerminalPath[[:blank:]]*\=[[:blank:]]*\)[[:print:]]*urfd.terminal'\1${URFD_CONFIG_DIR}/urfd.terminal'g" ${URFD_CONFIG_TMP_DIR}/urfd.ini
-  sed -i "s/\(PageOptions\['IRCDDB'\]\['Show'\][[:blank:]]*\=[[:blank:]]*\)[[:alpha:]]*/\1false/g" ${URFD_DASH_CONFIG}
-  sed -i "s,\(CallingHome\['HashFile'\][[:blank:]]*\=[[:blank:]]*\)\"\([[:print:]]*\)\",\1\"${URFD_CONFIG_DIR}/callinghome.php\",g" ${URFD_DASH_CONFIG} # move callinghome file to /config
-  sed -i "s,\(CallingHome\['LastCallHomefile'\][[:blank:]]*\=[[:blank:]]*\)\"\([[:print:]]*\)\",\1\"${URFD_CONFIG_DIR}\/lastcallhome.php\",g" ${URFD_DASH_CONFIG} # move lastcallhome file to /config
-  sed -i "s,\(CallingHome\['InterlinkFile'\][[:blank:]]*\=[[:blank:]]*\)\"\([[:print:]]*\)\",\1\"${URFD_CONFIG_DIR}\/xlxd.interlink\",g" ${URFD_DASH_CONFIG} # move xlxd.interlink file to /config
+  
+  if [[ ${SSL:-} == "true" ]]; then
+    __edit_value "DashboardUrl" "https:\/\/${URL}"
+
+  else
+    __edit_value "DashboardUrl" "http:\/\/${URL}:${PORT}"
+  
+  fi
+
+
+  __edit_value "Names" "Callsign" "${URFNUM}"
+  __edit_value "Names" "SysopEmail" "${EMAIL}"
+  __edit_value "Names" "Country" "${COUNTRY}"
+  __edit_value "Names" "Sponsor" "${DESCRIPTION}"
+  __edit_value "Modules" "Modules" "${MODULES}"
+  __edit_value "Modules" "DescriptionA" ${MODULEA}
+  __edit_property "DescriptionD" "DescriptionB"
+  __edit_value "Modules" "DescriptionB" "${MODULEB}"
+  __edit_property "DescriptionM" "DescriptionC"
+  __edit_value "Modules" "DescriptionC" "${MODULEC}"
+  __edit_property "DescriptionS" "DescriptionD"
+  __edit_value "Modules" "DescriptionD" "${MODULED}"
+  __line_comment "Modules" "DescriptionZ"
+  __edit_value "Brandmeister" "Enable" "${BRANDMEISTER}"
+  __edit_value "USRP" "Enable" "${ALLSTAR}"
+  __edit_value "USRP" "Callsign" "${CALLSIGN}"
+  __edit_value "USRP" "IPAddress" "${IP}"
+  __edit_value "YSF" "RegistrationID" "${YSFID}"
+  __edit_value "YSF" "RegistrationName" "${URFNUM}"
+  __edit_value "YSF" "RegistrationDescription" "${DESCRIPTION}"
+  __edit_value "DMR ID DB" "FilePath" "${URFD_CONFIG_DIR}/dmrid.dat"
+  __edit_value "NXDN ID DB" "FilePath" "${URFD_CONFIG_DIR}/nxdn.dat"
+  __edit_value "YSF TX\/RX DB" "FilePath" "${URFD_CONFIG_DIR}/ysfnode.dat"
+  __edit_value "Files" "XmlPath" "${URFD_CONFIG_DIR}/xlxd.xml"
+  __edit_value "Files" "WhitelistPath" "${URFD_CONFIG_DIR}/urfd.whitelist"
+  __edit_value "Files" "BlacklistPath" "${URFD_CONFIG_DIR}/urfd.blacklist"
+  __edit_value "Files" "InterlinkPath" "${URFD_CONFIG_DIR}/urfd.interlink"
+  __edit_value "Files" "G3TerminalPath" "${URFD_CONFIG_DIR}/urfd.terminal"
+
   rm -f ${URFD_CONFIG_TMP_DIR}/*d.service # get rid of systemd service
   rm -f ${URFD_CONFIG_TMP_DIR}/*d.mk # remove pre-compile configuration file
-  cp -vupn ${URFD_CONFIG_TMP_DIR}/* ${URFD_CONFIG_DIR}/ # don't overwrite config files if they exist
+  cp -vupn ${URFD_CONFIG_TMP_DIR}/* ${URFD_CONFIG_DIR}/ # don't overwrite config files if they exist in case they have been manually edited
   rm -rf ${URFD_CONFIG_TMP_DIR}
 
 fi
 
+
 # set timezone
 ln -snf /usr/share/zoneinfo/${TZ} /etc/localtime && echo ${TZ} > /etc/timezone
+
 
 # generate virtual host
 cat << EOF > /etc/apache2/sites-available/${URL}.conf
@@ -102,18 +269,22 @@ cat << EOF > /etc/apache2/sites-available/${URL}.conf
 </VirtualHost>
 EOF
 
+
 # Configure default timezone in php
 if [ ! -z ${TZ:-} ]; then
   echo "date.timezone = \""${TZ}"\"" >> /etc/php/*/apache2/php.ini
 
 fi
 
+
 # Configure httpd
 echo "Listen ${PORT}" >/etc/apache2/ports.conf
 echo "ServerName ${URL}" >> /etc/apache2/apache2.conf
 
+
 # disable default site(s)
 a2dissite *default >/dev/null 2>&1
+
 
 # enable xlxd dashboard
 a2ensite ${URL} >/dev/null 2>&1
