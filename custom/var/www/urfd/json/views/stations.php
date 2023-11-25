ed79b834561d486dc7928d1606d23236a2de64ed
Matt Fiscus <matt.fiscus@mac.com>
2023-11-25 09:55:02 -0700

    fixed inconsistencies with callsigns and suffixes in stations.php

--- custom/var/www/urfd/json/views/stations.php
+++ custom/var/www/urfd/json/views/stations.php
@@ -2,11 +2,17 @@
 
 /*** Add stations to payload ***/
 for ($i=0;$i<$Reflector->StationCount();$i++) {
+
+    $tmp = preg_split('/\s+/', $Reflector->Stations[$i]->GetCallSign(), -1, PREG_SPLIT_NO_EMPTY);
+    $Callsign = $tmp[0];
+
+    $tmp = preg_split('/\s+/', $Reflector->Stations[$i]->GetVia(), -1, PREG_SPLIT_NO_EMPTY);
+    $CallsignSuffix = $tmp[1];
 	
     // craft payload array
     $payload['stations'][$i] = array(
-        'callsign'       => $Reflector->Stations[$i]->GetCallSign(),
-        'callsignsuffix' => $Reflector->Stations[$i]->GetSuffix(),
+        'callsign'       => $Callsign,
+        'callsignsuffix' => $CallsignSuffix,
         'vianode'        => $Reflector->Stations[$i]->GetVia(),
         'onmodule'       => $Reflector->Stations[$i]->GetModule(),
         'lastheard'      => date('Y-m-d\TH:i:sp', $Reflector->Stations[$i]->GetLastHeardTime())
